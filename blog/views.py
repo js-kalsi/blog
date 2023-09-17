598b9e799b28dd04335fbaa3e00a273a6028f2af
Jaspreet Singh Kalsi <bugsbunny@MacBook-Pro-2.local>
2023-09-17 01:25:26 -0400

    added user accounts functionality

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
+from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .models import Post
 
@@ -13,3 +15,22 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
     context_object_name = "post"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    context_object_name = 'post_new'
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("home")
